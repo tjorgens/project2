0308acad2a3d2061700751ca4bb0e5a3bb4c8414
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 14:43:30 +0000

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skills|
+  Skill.create!(
+    title: "Rails #{skills}",
+    percent: 15
+  )
+end
+  
+  puts "5 skills created"
+  
+  9.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio Title #{portfolio_item}",
+      subtitle: "My Great Service",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      main_image: "https://placeholdit.co//i/300x150?&bg=fdg495&fc=8540df&text=Main Image",
+      thumb_image: "https://placeholdit.co//i/300x150?&bg=9538dj&fc=ej392j&text=Thumb Image",
+    )
+  end
+  
+  puts "9 portfolio items created"
