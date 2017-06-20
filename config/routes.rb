26e4701828dd1028c47fd4f7d6e19fd21252518a
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:31:59 +0000

    new portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,7 @@ Rails.application.routes.draw do
 
   get 'page/contact'
 
-  get 'pages/home'
-  
-  get 'pages/about'
-  
-  get 'pages/contact'
-  
+
   resources :blogs 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
