131f099ae2ae8473533010c55dd74478c5085f45
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:05:43 +0000

    portfolio image defaults set

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
     
   scope :ruby, -> { where(subtitle: 'Ruby on Rails') }
+  
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.main_image ||= "http://dfp3r41v9wmfi.cloudfront.net/wp-content/uploads/emperor_chicks.jpg"
+    self.thumb_image ||= "http://www.bunnyslippers.com/blog/wp-content/uploads/2013/11/rabbit-book-glasses.jpg"
+  end
 end
