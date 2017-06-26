7699b1b650b6d41fc4185396bf22275eff75ec92
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 16:42:45 +0000

    sign in running

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'about', to: 'page#about'
-  get 'contact', to: 'page#contact'
 
-  resources :blogs do 
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs do
     member do
       get :toggle_status
     end
   end
   
-  root to: 'page#home'
+  root to: 'pages#home'
 end
